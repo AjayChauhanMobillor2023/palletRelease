b731e4dce026cdb92f057b5f375e5727b4b9c625
Ajay Chauhan <ajay.chauhan@mobillor.com>
2024-10-21 11:56:08 +0530

    pallet release library

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,18 @@ pluginManagement {
         }
 }
 dependencyResolutionManagement {
+
         repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
         repositories {
                 google()
                 mavenCentral()
+                maven {
+
+                        url = uri("https://jitpack.io")
+                }
         }
 }
+
 
 rootProject.name = "PalletRelease"
 include(":app")
